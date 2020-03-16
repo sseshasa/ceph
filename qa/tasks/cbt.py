62baed0de34d6f98208b0d58e3ac9c1de229da2d
Sridhar Seshasayee <sseshasa@redhat.com>
2020-03-16 13:02:46 +0530

    qa/cbt: wait pg autoscaler testing
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>

--- qa/tasks/cbt.py
+++ qa/tasks/cbt.py
@@ -174,8 +174,8 @@ class CBT(Task):
 
     def checkout_cbt(self):
         testdir = misc.get_testdir(self.ctx)
-        repo = self.config.get('repo', 'https://github.com/ceph/cbt.git')
-        branch = self.config.get('branch', 'master')
+        repo = self.config.get('repo', 'https://github.com/sseshasa/cbt.git')
+        branch = self.config.get('branch', 'wip-cbt-librbdfio-waitpgautoscaler')
         branch = self.config.get('force-branch', branch)
         sha1 = self.config.get('sha1')
         if sha1 is None:
